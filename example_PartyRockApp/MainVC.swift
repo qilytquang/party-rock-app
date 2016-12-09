8298e865113fe5d622e7e6d6d84e7dc664376023
Quang Nguyen <qilytquang@gmail.com>
2016-12-09 22:13:46 +0700

    example Party Rock App: completed designing the UI

---
+++ example_PartyRockApp/MainVC.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  example_PartyRockApp
+//
+//  Created by quang nguyen on 12/9/16.
+//  Copyright © 2016 quang nguyen. All rights reserved.
+//
+
+import UIKit
+
+class MainVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+    }
+
+
+}
+
